62a61f79791eb2d684bc14618f66e358dcf7eec5
Valera Zhuk <valerazhukvalera@mail.ru>
2023-10-18 02:08:18 +0600

    feat: added footer and header

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <header class="w-full h-fit">
-
+  <header class="w-full h-fit flex flex-row items-center gap-x-4 py-6">
+    <img 
+      src="./../../assets/icons/main-logo.svg"
+      alt="Fast Service"
+      class="max-w-[40px] max-h-[40px] select-none"
+    />
+    <span class="text-mainDark text-2xl font-bold select-none">FastService</span>
   </header>
 </template>
